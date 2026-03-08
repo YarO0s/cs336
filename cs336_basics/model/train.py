ae116124076a0e425cebe55e19ff309c7eaa0641
yar.d.7772525@gmail.com <yard7@tuf.localdomain>
2026-03-08 20:01:40 +0300

    checkpointing

--- cs336_basics/model/train.py
+++ cs336_basics/model/train.py
@@ -1,7 +1,9 @@
 import argparse
 import sys
 import numpy as np
-import torch.nn.functional as nnfunc
+import wandb
+import os
+import time
 
 import cs336_basics.model.modules.func as f
 import cs336_basics.model.modules.modules as m
@@ -27,6 +29,8 @@ def main(args: list[str]) -> None:
     parser.add_argument("--batch", type=int)
     parser.add_argument("--device", type=str)
     parser.add_argument("--steps", type=int)
+    parser.add_argument("--checkp", type=int)
+    parser.add_argument("--checkp_out", type=str)
 
     config = parser.parse_args(args)
     train(config)
@@ -53,24 +57,48 @@ def train(config):
         config.eps
     )
 
-    for t in range(config.steps):
-        train, expected = l.get_batch(
-            data,
-            config.batch,
-            config.context,
-            config.device
-        )
+    with wandb.init(entity="", project="test-overfit") as run:
+        run.config.learning_rate = config.lr
+        run.config.vocab = config.vocab
+        run.config.d_model = config.dmod
+        run.config.context = config.context
+        run.config.num_heads = config.heads
+        run.config.d_ff = config.dff
+        run.config.num_layers = config.layers
+        run.config.theta = config.theta
+        run.config.device = config.device
+        run.config.weight_decay = config.wd
+        run.config.beta_one = config.beta1
+        run.config.beta_two = config.beta2
+        run.config.eps = config.eps
 
-        predicted = model.forward(train)
-        loss = f.cross_entropy(
-            predicted.view((predicted.shape[0] * predicted.shape[1], predicted.shape[2])),
-            expected.view((predicted.shape[0] * predicted.shape[1]))
-        )
-        print(f"{t}: {loss}")
+        for t in range(config.steps):
+            train, expected = l.get_batch(
+                data,
+                config.batch,
+                config.context,
+                config.device
+            )
 
-        opt.zero_grad()
-        loss.backward()
-        opt.step(device=config.device)
+            predicted = model.forward(train)
+            loss = f.cross_entropy(
+                predicted.view((predicted.shape[0] * predicted.shape[1], predicted.shape[2])),
+                expected.view((predicted.shape[0] * predicted.shape[1]))
+            )
 
+            run.log({"loss": loss})
+
+            opt.zero_grad()
+            loss.backward()
+            opt.step(device=config.device)
+
+            if t % config.checkp == 0:
+                print(f"Checkpointing on step {t}")
+
+                out_file = os.path.join(config.checkp_out, f"check_{int(time.time())}_{t}")
+                f.save_checkpoint(model, opt, t, out_file)
+
+        out_file = os.path.join(config.checkp_out, f"check_final_{int(time.time())}_{0}")
+        f.save_checkpoint(model, opt, 0, out_file)
 
 main(sys.argv[1:])
